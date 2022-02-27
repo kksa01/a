909ba8141ca41c09d6b732ab3e0437402707b9c3
Saad <sdosry@gmail.com>
2022-02-27 08:56:29 +0300

    first commat

---
+++ diff.c
@@ -0,0 +1,20 @@
+--- a	2022-01-24 11:45:05.615143100 +0300
++++ b	2022-01-24 11:47:22.035143100 +0300
+@@ -1,7 +1,7 @@
+ # This file may be used to create an environment using:
+ # $ conda create --name <env> --file <this file>
+ # platform: linux-64
+-@EXPLICIT
++EXPLICIT
+ 
+ https://repo.anaconda.com/pkgs/main/linux-64/_libgcc_mutex-0.1-main.conda
+ https://repo.anaconda.com/pkgs/main/linux-64/ca-certificates-2021.5.25-h06a4308_1.conda
+@@ -20,6 +20,7 @@
+ https://repo.anaconda.com/pkgs/main/linux-64/sqlite-3.35.4-hdfb4753_0.conda
+ https://repo.anaconda.com/pkgs/main/linux-64/python-3.8.10-h12debd9_8.tar.bz2
+ https://repo.anaconda.com/pkgs/main/linux-64/certifi-2021.5.30-py38h06a4308_0.conda
+-https://repo.anaconda.com/pkgs/main/noarch/wheel-0.36.2-pyhd3eb1b0_0.conda
+ https://repo.anaconda.com/pkgs/main/linux-64/setuptools-52.0.0-py38h06a4308_0.conda
+ https://repo.anaconda.com/pkgs/main/linux-64/pip-21.1.2-py38h06a4308_0.conda
++
++https:\\ here
